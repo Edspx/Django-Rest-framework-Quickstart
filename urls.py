764d9fc29a908cd209944f551ceae5c4d3d62a7a
Edspx <52833958+Edspx@users.noreply.github.com>
2021-01-24 12:34:46 -0300

    Writing regular Django views using our Serializer

--- urls.py
+++ urls.py
@@ -22,9 +22,11 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('tutorial.snippets.urls')),
 ]
